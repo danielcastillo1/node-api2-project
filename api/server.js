9e74781d8c114bfe8f7db7605551d0827e101597
Daniel Castillo <danielcastillo89@icloud.com>
2023-07-10 17:10:12 -0500

    created server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+
+server.use("*", (req, res) => {
+    res.status(400).json({
+        message: "not found"
+    })
+})
+
+module.exports = server
